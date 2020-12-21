93027dada5799b50c336b36a6991eddac31cb6d6
fujinjie <smilepassed@163.com>
2020-12-21 18:08:19 +0800

    add allure

--- test_allure.py
+++ test_allure.py
@@ -1,19 +1,24 @@
 import pytest
 import allure
 
+@allure.feature("模块测试demo")
 class TestOne:
 
+    @allure.story("case01 测试add")
     @pytest.mark.parametrize(("a","b"),((1,2),(2,3)))
     def test_add(self,a,b):
-        print('测试a+b')
-        sum = a + b
-        assert sum == a + b
+        with allure.step("a与b相加"):
+            print('测试a+b')
+            sum = a + b
+            assert sum == a + b
 
+    @allure.story("case02 测试product")
     @pytest.mark.parametrize(("a", "b"), ((1, 2), (2, 3)))
     def test_product(self,a,b):
-        print('测试a*b')
-        p = a * b
-        assert p == a * b
+        with allure.step("a与b相乘"):
+            print('测试a*b')
+            p = a * b
+            assert p == a * b
 
 
 if __name__ =='__main__':
